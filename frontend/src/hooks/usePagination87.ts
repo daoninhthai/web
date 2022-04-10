575bae2fd5d27b748b8ef58f9b18f1c3f98a3187
daoninhthai <thaimeo1131@gmail.com>
2022-04-10 23:34:56 +0700

    fix loi sort du lieu

---
+++ frontend/src/hooks/usePagination87.ts
@@ -0,0 +1,54 @@
+import { useState, useMemo } from 'react';
+
+interface PaginationResult<T> {
+    currentPage: number;
+    totalPages: number;
+    pageSize: number;
+    paginatedItems: T[];
+    goToPage: (page: number) => void;
+    nextPage: () => void;
+    prevPage: () => void;
+    setPageSize: (size: number) => void;
+}
+
+/**
+ * Custom hook for client-side pagination.
+ *
+ * @param items - Full array of items
+ * @param initialPageSize - Items per page (default: 10)
+ * @returns Pagination controls and current page items
+ */
+export function usePagination87<T>(
+    items: T[],
+    initialPageSize: number = 10
+): PaginationResult<T> {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(initialPageSize);
+
+    const totalPages = Math.ceil(items.length / pageSize);
+
+    const paginatedItems = useMemo(() => {
+        const start = (currentPage - 1) * pageSize;
+        return items.slice(start, start + pageSize);
+    }, [items, currentPage, pageSize]);
+
+    const goToPage = (page: number) => {
+        setCurrentPage(Math.max(1, Math.min(page, totalPages)));
+    };
+
+    const nextPage = () => goToPage(currentPage + 1);
+    const prevPage = () => goToPage(currentPage - 1);
+
+    return {
+        currentPage,
+        totalPages,
+        pageSize,
+        paginatedItems,
+        goToPage,
+        nextPage,
+        prevPage,
+        setPageSize,
+    };
+}
+
+export default usePagination87;
