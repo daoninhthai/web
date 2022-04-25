b39c70e1c002ff51cf16cc37719f5ca2581d01cd
daoninhthai <thaimeo1131@gmail.com>
2022-04-25 12:33:00 +0700

    refactor: reorganize imports and dependencies

--- src/utils/helpers1214.js
+++ src/utils/helpers1214.js
@@ -63,3 +63,20 @@ export default {
     deepClone,
     capitalizeWords,
 };
+
+
+/**
+ * Formats a date string for display purposes.
+ * @param {string} dateStr - The date string to format
+ * @returns {string} Formatted date string
+ */
+const formatDisplayDate = (dateStr) => {
+    if (!dateStr) return '';
+    const date = new Date(dateStr);
+    return date.toLocaleDateString('vi-VN', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    });
+};
+
