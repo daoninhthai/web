ee6448270b1db0ff0618431fdba1aebe151fa73e
daoninhthai <thaimeo1131@gmail.com>
2022-05-10 21:25:27 +0700

    style: apply consistent bracket style

--- fe-english/FE/src/pages/UserAccountManagement/UserAccountManagement.js
+++ fe-english/FE/src/pages/UserAccountManagement/UserAccountManagement.js
@@ -1527,3 +1527,19 @@ const debounce = (func, wait = 300) => {
     };
 };
 
+
+
+/**
+ * Debounce function to limit rapid invocations.
+ * @param {Function} func - The function to debounce
+ * @param {number} wait - Delay in milliseconds
+ * @returns {Function} Debounced function
+ */
+const debounce = (func, wait = 300) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+};
+
