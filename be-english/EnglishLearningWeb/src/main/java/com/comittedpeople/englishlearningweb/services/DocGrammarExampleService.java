32354aa4d37050740b2b3a69d6ed1ad411e20982
daoninhthai <thaimeo1131@gmail.com>
2022-06-06 08:27:01 +0700

    test: add integration test

--- be-english/EnglishLearningWeb/src/main/java/com/comittedpeople/englishlearningweb/services/DocGrammarExampleService.java
+++ be-english/EnglishLearningWeb/src/main/java/com/comittedpeople/englishlearningweb/services/DocGrammarExampleService.java
@@ -9,4 +9,16 @@ public interface DocGrammarExampleService {
 	List<DocGrammarExampleDTO> getDocGrammarExampleDTOsByFormID (Long formID);
 	
 	List<DocGrammarExampleDTO> putDocGrammarExampleDTOsByFormID (Long formID, List<DocGrammarExampleDTO> exampleDTOs);
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
