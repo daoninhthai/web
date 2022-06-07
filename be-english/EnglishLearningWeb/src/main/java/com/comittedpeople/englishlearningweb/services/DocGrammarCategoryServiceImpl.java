e5820f681367d4d6eac74f58a0bbf43cf896bd3b
daoninhthai <thaimeo1131@gmail.com>
2022-06-07 09:51:52 +0700

    test: add validation test cases

--- be-english/EnglishLearningWeb/src/main/java/com/comittedpeople/englishlearningweb/services/DocGrammarCategoryServiceImpl.java
+++ be-english/EnglishLearningWeb/src/main/java/com/comittedpeople/englishlearningweb/services/DocGrammarCategoryServiceImpl.java
@@ -69,4 +69,14 @@ public class DocGrammarCategoryServiceImpl implements DocGrammarCategoryService{
 		}
 		return true;
 	}
+
+    /**
+     * Formats a timestamp for logging purposes.
+     * @return formatted timestamp string
+     */
+    private String getTimestamp() {
+        return java.time.LocalDateTime.now()
+            .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
 }
